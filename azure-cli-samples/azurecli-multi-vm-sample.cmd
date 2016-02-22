d77107907db22c4867ab437e53e649cd52721931
Mike Wasson <mwasson@microsoft.com>
2016-02-22 15:46:47 -0800

    Add LB rules and probe

--- azure-cli-samples/azurecli-multi-vm-sample.cmd
+++ azure-cli-samples/azurecli-multi-vm-sample.cmd
@@ -1,4 +1,4 @@
-ECHO OFF
+::ECHO OFF
 SETLOCAL
 
 IF "%~1"=="" (
@@ -15,6 +15,8 @@ SET ENVIRONMENT=dev
 SET USERNAME=testuser
 SET PASSWORD=AweS0me@PW
 
+SET NUM_VM_INSTANCES=2
+
 :: Explicitly set the subscription to avoid confusion as to which subscription
 :: is active/default
 SET SUBSCRIPTION=%1
@@ -26,10 +28,12 @@ SET AVAILSET_NAME=%APP_NAME%-as
 SET LB_NAME=%APP_NAME%-lb
 SET LB_FRONTEND_NAME=%LB_NAME%-frontend
 SET LB_BACKEND_NAME=%LB_NAME%-backend-pool
+SET LB_PROBE_NAME=%LB_NAME%-probe
 SET IP_NAME=%APP_NAME%-pip
 SET NSG_NAME=%APP_NAME%-nsg
 SET SUBNET_NAME=%APP_NAME%-subnet
 SET VNET_NAME=%APP_NAME%-vnet
+SET DIAGNOSTICS_STORAGE=%APP_NAME:-=%diag
 
 :: For Windows, use the following command to get the list of URNs:
 :: azure vm image list %LOCATION% MicrosoftWindowsServer WindowsServer 2012-R2-Datacenter
@@ -39,8 +43,7 @@ SET WINDOWS_BASE_IMAGE=MicrosoftWindowsServer:WindowsServer:2012-R2-Datacenter:4
 SET VM_SIZE=Standard_DS1
 
 :: Set up the postfix variables attached to most CLI commands
-SET POSTFIX=--resource-group %RESOURCE_GROUP% --location %LOCATION% ^
-  --subscription %SUBSCRIPTION%
+SET POSTFIX=--resource-group %RESOURCE_GROUP% --subscription %SUBSCRIPTION%
 
 CALL azure config mode arm
 
@@ -48,51 +51,72 @@ CALL azure config mode arm
 :: Create resources
 
 :: Create the enclosing resource group
-CALL azure group create --name %RESOURCE_GROUP% --location %LOCATION%
+CALL azure group create --name %RESOURCE_GROUP% --location %LOCATION% ^
+  --subscription %SUBSCRIPTION%
 
 :: Create the availability set
-CALL azure availset create --name %AVAILSET_NAME% %POSTFIX%
+CALL azure availset create --name %AVAILSET_NAME% --location %LOCATION% %POSTFIX%
 
 :: Create the network security group
-CALL azure network nsg create --name %NSG_NAME% %POSTFIX%
+CALL azure network nsg create --name %NSG_NAME% --location %LOCATION% %POSTFIX%
 
 :: Create the VNet
 CALL azure network vnet create --address-prefixes 10.0.0.0/16 ^
-  --name %VNET_NAME% %POSTFIX%
+  --name %VNET_NAME% --location %LOCATION% %POSTFIX%
 
 :: Create the subnet
 CALL azure network vnet subnet create --vnet-name %VNET_NAME% --address-prefix ^
   10.0.0.0/24 --name %SUBNET_NAME% --network-security-group-name %NSG_NAME% ^
-  --resource-group %RESOURCE_GROUP% --subscription %SUBSCRIPTION%
+  %POSTFIX%
 
 :: Create the public IP address (dynamic)
-CALL azure network public-ip create --name %IP_NAME% %POSTFIX%
+CALL azure network public-ip create --name %IP_NAME% --location %LOCATION% %POSTFIX%
+
+:: Create the storage account for diagnostics logs
+CALL azure storage account create --type LRS --location %LOCATION% %POSTFIX% ^
+  %DIAGNOSTICS_STORAGE%
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Load balancer
 
 :: Create the load balancer
-CALL azure network lb create --name %LB_NAME% %POSTFIX%
+CALL azure network lb create --name %LB_NAME% --location %LOCATION% %POSTFIX%
 
 :: Create LB front-end and associate it with the public IP address
 CALL azure network lb frontend-ip create --name %LB_FRONTEND_NAME% --lb-name ^
-  %LB_NAME% --public-ip-name %IP_NAME% --resource-group %RESOURCE_GROUP% ^
-  --subscription %SUBSCRIPTION%
+  %LB_NAME% --public-ip-name %IP_NAME% %POSTFIX%
 
 :: Create LB back-end address pool
 CALL azure network lb address-pool create --name %LB_BACKEND_NAME% --lb-name ^
-  %LB_NAME% --resource-group %RESOURCE_GROUP% --subscription %SUBSCRIPTION%
+  %LB_NAME% %POSTFIX%
 
-:: Allow RDP
-CALL azure network nsg rule create -g %RESOURCE_GROUP% --nsg-name %NSG_NAME% ^
+:: Create a health probe for an HTTP endpoint
+CALL azure network lb probe create --name %LB_PROBE_NAME% --lb-name %LB_NAME% ^
+  --port 80 --interval 5 --count 2 --protocol http --path / %POSTFIX%
+
+:: Create a load balancer rule for HTTP
+CALL azure network lb rule create --name %LB_NAME%-rule-http --protocol tcp ^
+  --lb-name %LB_NAME% --frontend-port 80 --backend-port 80 --frontend-ip-name ^
+  %LB_FRONTEND_NAME% --probe-name %LB_PROBE_NAME% %POSTFIX%
+
+:: Create NSG rule to allow RDP
+CALL azure network nsg rule create --nsg-name %NSG_NAME% ^
   --direction Inbound --protocol Tcp --destination-port-range 3389 ^
-  --source-port-range * --priority 100 --access Allow RDPAllow
+  --source-port-range * --priority 100 --access Allow RDPAllow %POSTFIX%
 
+:: Create NSG rule to allow HTTP
+CALL azure network nsg rule create --nsg-name %NSG_NAME% ^
+  --direction Inbound --protocol Tcp --destination-port-range 80 ^
+  --source-port-range * --priority 200 --access Allow HttpAllow %POSTFIX%
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Create VMs and per-VM resources
-
-FOR /L %%I IN (0,1,1) DO CALL :CreateVM %%I
+FOR /L %%I IN (1,1,%NUM_VM_INSTANCES%) DO CALL :CreateVM %%I
 
 GOTO :eof
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Subroutine to create the VMs
+:: Subroutine to create the VMs and per-VM resources
 
 :CreateVm
 
@@ -101,34 +125,28 @@ ECHO Creating VM %1
 SET VM_NAME=%APP_NAME%-vm%1
 SET NIC_NAME=%VM_NAME%-0nic
 SET VHD_STORAGE=%VM_NAME:-=%st0
-SET DIAGNOSTICS_STORAGE=%VM_NAME:-=%diag
 SET /a RDP_PORT=50001 + %1
 
 :: Create NIC for VM1
 CALL azure network nic create --name %NIC_NAME% --subnet-name %SUBNET_NAME% ^
-  --subnet-vnet-name %VNET_NAME% %POSTFIX%
+  --subnet-vnet-name %VNET_NAME% --location %LOCATION% %POSTFIX%
 
 :: Add NIC to back-end address pool
 CALL azure network nic address-pool add --name %NIC_NAME% --lb-name %LB_NAME% ^
-  --lb-address-pool-name %LB_BACKEND_NAME% --resource-group %RESOURCE_GROUP% ^
-  --subscription %SUBSCRIPTION%
+  --lb-address-pool-name %LB_BACKEND_NAME% %POSTFIX%
 
 :: Create NAT rule for RDP
 CALL azure network lb inbound-nat-rule create --name rdp-vm%1 --frontend-port ^
   %RDP_PORT% --backend-port 3389 --lb-name %LB_NAME% --frontend-ip-name ^
-  %LB_FRONTEND_NAME% --resource-group %RESOURCE_GROUP% ^
-  --subscription %SUBSCRIPTION%
+  %LB_FRONTEND_NAME% %POSTFIX%
 
 :: Add NAT rule to the NIC
 CALL azure network nic inbound-nat-rule add --name %NIC_NAME% --lb-name ^
-  %LB_NAME% --lb-inbound-nat-rule-name rdp-vm%1 --resource-group %RESOURCE_GROUP% ^
-  --subscription %SUBSCRIPTION%
+  %LB_NAME% --lb-inbound-nat-rule-name rdp-vm%1 %POSTFIX%
 
 :: Create the storage account for the OS VHD
-CALL azure storage account create --type PLRS %POSTFIX% %VHD_STORAGE%
-
-:: Create the storage account for diagnostics logs
-CALL azure storage account create --type LRS %POSTFIX% %DIAGNOSTICS_STORAGE%
+CALL azure storage account create --type PLRS --location %LOCATION% ^
+ %VHD_STORAGE% %POSTFIX%
 
 :: Create the VM
 CALL azure vm create --name %VM_NAME% --os-type Windows --image-urn ^
@@ -137,11 +155,10 @@ CALL azure vm create --name %VM_NAME% --os-type Windows --image-urn ^
   %VHD_STORAGE% --os-disk-vhd "%VM_NAME%-osdisk.vhd" --admin-username ^
   "%USERNAME%" --admin-password "%PASSWORD%" --boot-diagnostics-storage-uri ^
   "https://%DIAGNOSTICS_STORAGE%.blob.core.windows.net/" --availset-name ^
-  %AVAILSET_NAME% %POSTFIX%
+  %AVAILSET_NAME% --location %LOCATION% %POSTFIX%
 
 :: Attach a data disk
 CALL azure vm disk attach-new --vm-name %VM_NAME% --size-in-gb 128 --vhd-name ^
-  %VM_NAME%-data1.vhd --storage-account-name %VHD_STORAGE% --resource-group ^
-  %RESOURCE_GROUP% --subscription %SUBSCRIPTION%
+  %VM_NAME%-data1.vhd --storage-account-name %VHD_STORAGE% %POSTFIX%
 
 goto :eof
